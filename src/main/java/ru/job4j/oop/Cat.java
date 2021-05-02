0da3fb4a11e3a330686c3d9e94168ec1918674c3
ooo <0>
2021-05-02 13:55:42 +0300

    добавлен класс Cat

---
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -0,0 +1,32 @@
+package ru.job4j.oop;
+
+public class Cat {
+
+    private String food;
+    private String name;
+
+    public void giveNick(String nick){
+        this.name = nick;
+    }
+
+    public void show() {
+        System.out.println(this.food);
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("There are gav's food.");
+        Cat gav = new Cat();
+        gav.giveNick("Gav");
+        gav.eat(gav.name + " kotleta");
+        gav.show();
+        System.out.println("There are black's food.");
+        Cat black = new Cat();
+        black.giveNick("Black");
+        black.eat(black.name + " fish");
+        black.show();
+    }
+}
