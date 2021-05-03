ec1224686cef35276b266bfb411e6dbd3622d69b
ooo <0>
2021-05-03 21:11:47 +0300

    обновлены классы Point и PointTest

--- src/test/java/ru/job4j/oop/PointTest.java
+++ src/test/java/ru/job4j/oop/PointTest.java
@@ -7,7 +7,7 @@ import static org.hamcrest.Matchers.is;
 
 public class PointTest {
     @Test
-    public void WhenFirstPont11AndSecondPoint11(){
+    public void WhenFirstPoint11AndSecondPoint11(){
         Point a = new Point(1, 2);
         Point b = new Point(1, 1);
         double result = a.distanceTo(b);
@@ -16,10 +16,19 @@ public class PointTest {
     }
 
     @Test
-    public void WhenFirstPont01AndSecondPoint11(){
+    public void WhenFirstPoint01AndSecondPoint11(){
         Point a = new Point(0, 1);
         Point b = new Point(1, 1);
         double result = a.distanceTo(b);
+        double expected = 1.0;
+        Assert.assertThat(result, is(expected));
+    }
+
+    @Test
+    public void WhenFirstPoint010AndSecondPoint011(){
+        Point a = new Point(0, 1);
+        Point b = new Point(1, 1);
+        double result = a.distance3d(b);
         double expected = 1.0;
         Assert.assertThat(result, is(expected));
     }
