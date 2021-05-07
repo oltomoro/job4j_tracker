647ee7115ff22a3d4a3966819ee03dbf6e188d6c
ooo <0>
2021-05-07 21:40:04 +0300

    Обновлены классы Tracker и StartUI

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -3,13 +3,19 @@ package ru.job4j.tracker;
 import java.time.format.DateTimeFormatter;
 
 public class StartUI {
-    public void createItem(){
-        Item item = new Item();
-        String formattedDate = item.getCreated().format(DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss"));
-        System.out.println(formattedDate);
-    }
+//    public void createItem(){
+//        Item item = new Item();
+//        String formattedDate = item.getCreated().format(DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss"));
+//        System.out.println(formattedDate);
+//    }
 
     public static void main(String[] args) {
-        new StartUI().createItem();
+        Item item = new Item(1, "test");
+        Tracker tracker = new Tracker();
+        tracker.add(item);
+        Item foundItem = tracker.findById(1);
+        System.out.println("Id заявки: " + foundItem.getId());
+        System.out.println("Имя заявки: " + foundItem.getName());
+        System.out.println("Дата создания заявки: " + foundItem.getCreated());
     }
 }
--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,16 +1,50 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
-    private final Item[] items = new Item[100];
+    /**
+     * Максимальное количество заявок в трекере
+     */
+    private final int ITEMS_COUNT = 100;
+
+    /**
+     * Массив содержащий в себе все заявки
+     */
+    private final Item[] items = new Item[ITEMS_COUNT];
+
+    /**
+     * Начальный индекс для id
+     */
     private int ids = 1;
+
+    /**
+     * Количество заявок в трекере
+     */
     private int size = 0;
 
+    /**
+     * Добавляет новую заявку в массив заявок
+     *
+     * @param item добавляемая заявка
+     * @return заявка
+     */
     public Item add(Item item) {
-        item.setId(ids++);
-        items[size++] = item;
+        if (size < ITEMS_COUNT) {
+            item.setId(ids++);
+            items[size++] = item;
+        } else {
+            throw new ArrayIndexOutOfBoundsException(String.format("Превышено максимальное количество заявок (%s шт.)", ITEMS_COUNT));
+        }
         return item;
     }
 
+    /**
+     * Находит в массиве заявку по id
+     *
+     * @param id - id искомой заявки
+     * @return объект найденной заявки, если ничего не найдено, то null
+     */
     public Item findById(int id) {
         Item rsl = null;
         for (int index = 0; index < size; index++) {
@@ -21,5 +55,37 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    /**
+     * Находит все заявки в массиве
+     *
+     * @return массив найденных заявок
+     */
+    public Item[] findAll() {
+        int index = 0;
+        for (int i = 0; i < this.items.length; i++) {
+            if (this.items[i] == null) {
+                index = i;
+                break;
+            }
+        }
+        return Arrays.copyOfRange(this.items, 0, index);
+    }
+
+    /**
+     * Находит все заявки, c заданным именем, в массиве
+     *
+     * @return массив найденных заявок
+     */
+    public Item[] findByName(String name){
+        int index = 0;
+        for (int i = 0; i < this.items.length; i++) {
+            if (this.items[i].getName().equals(name)) {
+                index = i;
+                break;
+            }
+        }
+        return Arrays.copyOfRange(this.items, 0, index + 1);
     }
 }
