a2ecd9f0b005e2191912648b20960380b0a84da1
ooo <0>
2021-05-20 12:16:45 +0300

    добавлен класс MagicBall

---
+++ src/main/java/ru/job4j/io/MagicBall.java
@@ -0,0 +1,18 @@
+package ru.job4j.io;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class MagicBall {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Я великий Оракул. Что ты хочешь узнать? ");
+        String question = input.nextLine();
+        String answer = switch (new Random().nextInt(3)) {
+            case 0 -> "Да";
+            case 1 -> "Нет";
+            default -> "Может быть";
+        };
+        System.out.println("Ответ на вопрос: " + question + " = " + answer);
+    }
+}
