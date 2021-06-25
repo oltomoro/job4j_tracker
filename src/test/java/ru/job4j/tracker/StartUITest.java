f3bea198a0c358b6432c3185f4fa4c9cb3186597
Vladimir <email>
2021-06-25 12:59:26 +0300

    [#182960] добавлены тесты для класса StartUI, методов whenDeleteItem() и whenReplaceItem()

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -5,6 +5,7 @@ import ru.job4j.tracker.input.Input;
 import ru.job4j.tracker.input.StubInput;
 
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.assertThat;
 
 public class StartUITest {
@@ -18,4 +19,43 @@ public class StartUITest {
         Item expected = new Item(questions[0]);
         assertThat(result.getName(), is(expected.getName()));
     }
+
+    @Test
+    public void whenReplaceItem(){
+        Tracker tracker = new Tracker();
+        Item item = new Item("first name");
+        tracker.add(item);
+        String newName = "new name";
+        String[] questions = {"1", newName};
+        Input input = new StubInput(questions);
+        StartUI.editItem(input, tracker);
+        String result = tracker.findById(1).getName();
+        assertThat(result, is(newName));
+    }
+
+    @Test
+    public void whenDeleteItem(){
+        Tracker tracker = new Tracker();
+        Item item = new Item("Test Item");
+        tracker.add(item);
+        String[] questions = {"1"};
+        Input input = new StubInput(questions);
+        StartUI.deleteItem(input, tracker);
+        Item result = tracker.findById(1);
+        assertThat(result, is(nullValue()));
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
