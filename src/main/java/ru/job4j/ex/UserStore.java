23fa580d7a1f6f728ca647c34d2261503712028c
Vladimir <email>
2021-08-26 20:57:58 +0300

    [#219368] Добавлены классы User, UserInvalidException, UserNotFoundException и UserStore

---
+++ src/main/java/ru/job4j/ex/UserStore.java
@@ -0,0 +1,39 @@
+package ru.job4j.ex;
+
+public class UserStore {
+    public static User findUser(User[] users, String login) throws UserNotFoundException {
+        User user = null;
+        for (User u : users) {
+            if (u.getUsername().equals(login)) {
+                user = u;
+            }
+        }
+        if (user == null) {
+            throw new UserNotFoundException(String.format("User [%s] not found", login));
+        }
+        return user;
+    }
+
+    public static boolean validate(User user) throws UserInvalidException {
+        if (!user.isValid() || user.getUsername().length() < 3){
+            throw new UserInvalidException(String.format("User [%s] not valid", user.getUsername()));
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        User[] users = {
+                new User("Petr Arsentev", false)
+        };
+        try {
+            User user = findUser(users, "Petr Arsentev");
+            if (validate(user)) {
+                System.out.println("This user has an access");
+            }
+        } catch (UserInvalidException uie) {
+            System.out.println(uie.getMessage());
+        } catch (UserNotFoundException unfe) {
+            System.out.println(unfe.getMessage());
+        }
+    }
+}
