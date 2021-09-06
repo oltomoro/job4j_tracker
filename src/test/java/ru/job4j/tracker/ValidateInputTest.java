630da337d244de55079f9e91e50e1abe237e8af9
Vladimir <email>
2021-09-06 18:28:42 +0300

    [#311887] тест на ValidateInputTest

---
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -0,0 +1,50 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+import ru.job4j.tracker.input.Input;
+import ru.job4j.tracker.input.StubInput;
+import ru.job4j.tracker.input.ValidateInput;
+import ru.job4j.tracker.output.Output;
+import ru.job4j.tracker.output.StubOutput;
+
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.collection.IsArrayContainingInOrder.arrayContaining;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class ValidateInputTest {
+    @Test
+    public void whenValidInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"1"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected, is(1));
+    }
+
+    @Test
+    public void whenMultipleValidInput() {
+        Output out = new StubOutput();
+        String[] expected = {"0","1","2"};
+        Input in = new StubInput(expected);
+        ValidateInput input = new ValidateInput(out, in);
+        String[] result = new String[3];
+        result[0] = String.valueOf(input.askInt("Enter menu:"));
+        result[1] = String.valueOf(input.askInt("Enter menu:"));
+        result[2] = String.valueOf(input.askInt("Enter menu:"));
+        assertThat(expected, arrayContaining(result));
+    }
+
+    @Test
+    public void whenInvalidInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"-1"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected, is(-1));
+    }
+}
