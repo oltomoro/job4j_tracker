7d1fc0f01900219aeb9c392011da597b4b314e69
Vladimir <email>
2021-12-01 17:25:55 +0300

    [#94606] Исправлен класс StaticTracker

--- src/main/java/ru/job4j/tracker/SingleTracker.java
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -1,54 +1,15 @@
 package ru.job4j.tracker;
 
 public class SingleTracker {
-    private static Tracker tracker = new Tracker();
+    private static Tracker tracker;
 
     private SingleTracker() {
     }
 
     public static Tracker getInstance() {
+        if (tracker == null) {
+            tracker = new Tracker();
+        }
         return tracker;
-    }
-
-    /**
-     * Метод обертка для Tracker.add(Item item)
-     */
-    public Item add(Item item) {
-        return tracker.add(item);
-    }
-
-    /**
-     * Метод обертка для Tracker.findById(int id)
-     */
-    public Item findById(int id) {
-        return tracker.findById(id);
-    }
-
-    /**
-     * Метод обертка для Tracker.findAll()
-     */
-    public Item[] findAll() {
-        return tracker.findAll();
-    }
-
-    /**
-     * Метод обертка для Tracker.findByName(String name)
-     */
-    public Item[] findByName(String name) {
-        return tracker.findByName(name);
-    }
-
-    /**
-     * Метод обертка для Tracker.replace(int id, Item item)
-     */
-    public boolean replace(int id, Item item) {
-        return tracker.replace(id, item);
-    }
-
-    /**
-     * Метод обертка для Tracker.delete(int id)
-     */
-    public boolean delete(int id) {
-        return tracker.delete(id);
     }
 }
